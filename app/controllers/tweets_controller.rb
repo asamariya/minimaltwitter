d6e8955b88fc8c9a364aa7a5e6e66ccbb538f604
Abhishek Samariya <asamariya@gmail.com>
2019-12-09 11:58:55 +1100

    Installed Ruby Twitter gem and now able to get signed in user's tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,15 @@
 class TweetsController < ApplicationController
   def index
+    if session[:token].present?
+      # get some tweets
+      client = Twitter::REST::Client.new do |config|
+        config.consumer_key        = "s9jW8vgWKYqEtwCv49X0M1HVc"
+        config.consumer_secret     = "yglUOrCowu8nSHft8CSgLoR1hCK9c8ZBjHWovwXFRBzu5h1Uw0"
+        config.access_token        = session[:token]
+        config.access_token_secret = session[:token_secret]
+      end
+      @tweets = client.home_timeline
+    end
   end
 
   def login
